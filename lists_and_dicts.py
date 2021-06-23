e83c777d75f662ccbe1bf99437c23f9942bbf5e5
Jara Eugenio, Yhojan Daniel <jhojandje@gmail.com>
2021-06-23 17:36:45 -0500

    Add files

---
+++ lists_and_dicts.py
@@ -0,0 +1,21 @@
+def main():
+    my_list = [1, "Hello", True, 4.5]
+    my_dict = {"firstname": "Yhojan Daniel","lastname": "Jara Eugenio"}
+
+    super_list =[
+        {"firstname": "Yhojan","lastname": "Jara"},
+        {"firstname": "Daniel","lastname": "Eugenio"}
+    ]
+
+    super_dict = {
+        "natural_nums": [1, 2, 3, 4, 5],
+        "integer_nums": [-1, 0, 1, 2, 3],
+        "float_nums":[2.5, 1.2, 10.7]
+    }
+
+    for key, value in super_dict.items():
+        print(f"Llave: {key} ; Valor: {value}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ numbers_squared.py
@@ -0,0 +1,22 @@
+def main():
+    list_squares = []
+    pointers_list = []
+    
+    pointer = 0
+    iterator = 1
+
+    for iterator in range(1,11):
+        solve = iterator**2
+        evaluator = solve % 3
+
+        if evaluator != 0:
+            pointers_list.append(pointer)
+            pointer += 1
+            list_squares.append(solve)
+            print(f"Using the number {iterator} in the list the solve is: {solve}")
+    
+    print(f"Values in ur list are : {list_squares}")
+    print(f"Pointersin ur list are: {pointers_list}")
+
+if __name__ == "__main__":
+    main()
