911ed500ace84ca37aeba975b68a877811790822
Jara Eugenio, Yhojan Daniel <jhojandje@gmail.com>
2021-06-25 09:15:35 -0500

    Add statement if i % 3 == 0

--- dicts_comprehesed.py
+++ dicts_comprehesed.py
@@ -3,9 +3,10 @@
 def main():
     new_dict = {}
     for iterator in range(1, 101):
-        solve = iterator**3
-        new_dict[iterator] = solve
-        print(f"Current iterator: {iterator} && Current value: {solve}")
+        if iterator % 3 == 0:
+            solve = iterator**3
+            new_dict[iterator] = solve
+            print(f"Current iterator: {iterator} && Current value: {solve}")
 
     print(new_dict)
 
