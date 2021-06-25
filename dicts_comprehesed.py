4ef99cf51bfd6ed3ca61437b582190b57556c900
Jara Eugenio, Yhojan Daniel <jhojandje@gmail.com>
2021-06-25 09:19:00 -0500

    It's Same but in oneline

--- dicts_comprehesed.py
+++ dicts_comprehesed.py
@@ -1,12 +1,14 @@
 # Dictionaries Comprehesed
 
 def main():
-    new_dict = {}
-    for iterator in range(1, 101):
-        if iterator % 3 != 0:
-            solve = iterator**3
-            new_dict[iterator] = solve
-            print(f"Current iterator: {iterator} && Current value: {solve}")
+    # new_dict = {}
+    # for iterator in range(1, 101):
+    #     if iterator % 3 != 0:
+    #         solve = iterator**3
+    #         new_dict[iterator] = solve
+    #         print(f"Current iterator: {iterator} && Current value: {solve}")
+
+    new_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
 
     print(new_dict)
 
