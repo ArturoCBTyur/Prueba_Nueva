058ca23706360a49a3e145b54398a02561fdaedc
Jara Eugenio, Yhojan Daniel <jhojandje@gmail.com>
2021-06-25 08:28:36 -0500

    Only logic structure

---
+++ dicts_comprehesed.py
@@ -0,0 +1,9 @@
+def main():
+    new_dict = {}
+    for iterator in range(1, 101):
+        solve = iterator**2
+        new_dict.update(iterator = solve)
+        print(f"Current iterator: {iterator} && Current value: {solve}")
+
+if __name__ == "__main__":
+    main()
