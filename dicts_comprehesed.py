361a7b88659ce499de7836d8d6ac7a247373f517
Jara Eugenio, Yhojan Daniel <jhojandje@gmail.com>
2021-06-25 09:16:24 -0500

    Change == to !=

--- dicts_comprehesed.py
+++ dicts_comprehesed.py
@@ -3,7 +3,7 @@
 def main():
     new_dict = {}
     for iterator in range(1, 101):
-        if iterator % 3 == 0:
+        if iterator % 3 != 0:
             solve = iterator**3
             new_dict[iterator] = solve
             print(f"Current iterator: {iterator} && Current value: {solve}")
