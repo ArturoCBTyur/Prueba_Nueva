5213026fab9b64bc8638db90624504f7c3c8470b
Jara Eugenio, Yhojan Daniel <jhojandje@gmail.com>
2021-06-24 22:56:06 -0500

    Add the prints in the Challenge

--- numbers_squared.py
+++ numbers_squared.py
@@ -22,9 +22,13 @@ def main():
 
     squares = [i**2 for i in range(1, 100000) if i % 3 != 0]
 
+    print(squares)
+
     # Python Task
 
     challenge = [j**2 for j in range(1, 101) if j % 4 != 0 if j % 6 != 0 if j % 9 != 0]
+
+    print(challenge)
 
     print("This is all...")
 
