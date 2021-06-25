fef9a202e06c2ddc92420b6b50c4d909ab166aaa
Jara Eugenio, Yhojan Daniel <jhojandje@gmail.com>
2021-06-25 08:55:31 -0500

    Prepared to merge to Hot_Fix_2

--- dicts_comprehesed.py
+++ dicts_comprehesed.py
@@ -3,6 +3,7 @@
 def main():
     new_dict = {}
     for iterator in range(1, 101):
+        
         solve = iterator**2
         new_dict.update(iterator = solve)
         print(f"Current iterator: {iterator} && Current value: {solve}")
