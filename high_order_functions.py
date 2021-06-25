ca514e42a41bd8d335ac01a5dd0310e98b0af8de
Jara Eugenio, Yhojan Daniel <jhojandje@gmail.com>
2021-06-25 11:39:19 -0500

    Without high order functions

---
+++ high_order_functions.py
@@ -0,0 +1,28 @@
+import functools # To reduce
+
+def main():
+    # First type function -> filter
+
+    # Without filter
+    my_list = [1,2,3,4,5,6,7]
+    output = [i**2 for i in my_list if i % 2 != 0]
+    print(output)
+
+    # Second type function -> map
+
+    # Without map
+    my_list_2 = [1,2,3,4,5,6,7]
+    output_2 = [i**2 for i in my_list]
+    print(output_2)
+
+    # Third type function -> filter
+
+    # Without reduce
+    my_list_3 = [1,2,3,4,5,6,7]
+    output_3 = 1
+    for i in my_list_3:
+        output_3 *= i
+    print(output_3)
+
+if __name__ == "__main__":
+    main()
